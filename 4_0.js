c48bf882c7a322e8e19f7efd0a88205535f7f898
ggang89 <ggang89@gmail.com>
2024-02-05 21:56:51 +0900

    노마드 js 4강

--- 4_0.js
+++ 4_0.js
@@ -6,12 +6,32 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 // const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
+//일반적으로 string만 포함된 변수는 대문자로 표기하고 string만 저장하고 싶을때 사용함
+//상대적으로 덜 중요한 정보를 저장하는 변수명이다
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-  //console.log("Hello",loginInput.value);
   event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  console.log(username);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
+
+//loginForm.addEventListener("submit", onLoginSubmit);
