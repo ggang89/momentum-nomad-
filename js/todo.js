1eb347b858a21417839dd0823dfe91412cabfc08
ggang89 <ggang89@gmail.com>
2024-02-12 23:25:35 +0900

    toDos라는 배열을 만들고,newTodo값을 넣어준다.->localstorage에 저장해준다 -> 저장된 값은 JSON.parse로 배열로 만들어준다.

--- js/todo.js
+++ js/todo.js
@@ -3,15 +3,22 @@ const toDoInput = document.querySelector("#todo-form input");
 //const toDoInput = toDoForm.querySelector("input");  2번식과 같은 내용
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
 const toDos = [];
+//todo를 작성해서 넣은 배열
 
 function saveToDos() {
-  localStorage.setItem("todos", toDos);
+  //localStorage에 todo를 저장해준다
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+  //JSON.stringify=모든 자바스크립트 코드를 string으로 만들어준다
 }
 
 function deleteToDo(event) {
   const li = event.target.parentElement;
+  //변수 li는 event가 발생한 target의 부모요소
   li.remove();
+  //변수 li를 제거한다
 }
 
 function paintToDo(newTodo) {
@@ -21,8 +28,10 @@ function paintToDo(newTodo) {
   const button = document.createElement("button");
   button.innerText = "❌";
   button.addEventListener("click", deleteToDo);
+  //button을 click하면 deleteToDo함수가 실행된다
   li.appendChild(span);
   li.appendChild(button);
+  //부모객체.appendChild(자식객체)
 
   toDoList.appendChild(li);
 }
@@ -32,8 +41,19 @@ function handleToDoSubmit(event) {
   const newTodo = toDoInput.value;
   toDoInput.value = "";
   toDos.push(newTodo);
+  //nexTodo를 toDos배열에 넣는다
   paintToDo(newTodo);
+  //화면에 nexTodo를 그려준다
   saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  //JSON.parse= javascript object로 만들어준다
+  parsedToDos.forEach((item) => console.log("this is the turn of ", item));
+  //parsedToDos가 가진 각각의 item의 함수를 실행할 때 -> forEach
+}
