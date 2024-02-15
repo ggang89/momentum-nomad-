bda3f641379fb846648ecd94f64ee061bd531ce8
ggang89 <ggang89@gmail.com>
2024-02-15 17:28:30 +0900

    wearher API 연결, fetch로 데이터 받아서 연결

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,17 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY ="686a73df8afa13ecc2c99704f6470090";
+
 function onGeoOk(position){
   const lat = position.coords.latitude;
   const lng = position.coords.longitude;
-  console.log("you live in",lat, lng);
-
+ const url= ` https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+ fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 function onGeoError(){
  alert("Can't find you. No weather for you");
